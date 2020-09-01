c9e8731f4d8b708818f7c8901f28b30e8ec11552
Andrei-ctrl <59820123+Andrei-ctrl@users.noreply.github.com>
2020-09-01 03:34:12 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,669 @@
+//data
+var manor = [
+  {
+    name: "Duvel",
+    price: "2.60",
+  },
+  {
+    name: "Warsteiner",
+    price: "1.85",
+  },
+  {
+    name: "Pietra",
+    price: "3.90",
+  },
+  {
+    name: "Pietra",
+    price: "8.20",
+  },
+  {
+    name: "Brewdog",
+    price: "2.20",
+  },
+  {
+    name: "GRIMBERGEN",
+    price: "1.95",
+  },
+  {
+    name: "Peroni",
+    price: "2.20",
+  },
+  {
+    name: "Chimay",
+    price: "8.50",
+  },
+  {
+    name: "Liefmans",
+    price: "9.00",
+  },
+  {
+    name: "Peroni",
+    price: "8.00",
+  },
+  {
+    name: "Bier Factory",
+    price: "13.60",
+  },
+  {
+    name: "GRIMBERGEN",
+    price: "11.50",
+  },
+  {
+    name: "Brewdog",
+    price: "2.95",
+  },
+  {
+    name: "Riegele",
+    price: "2.95",
+  },
+  {
+    name: "Hoppy People",
+    price: "3.50",
+  },
+  {
+    name: "Officina Della Birra",
+    price: "3.90",
+  },
+  {
+    name: "Brewdog",
+    price: "3.50",
+  },
+  {
+    name: "Desperados",
+    price: "2.80",
+  },
+  {
+    name: "Guinness",
+    price: "3.00",
+  },
+  {
+    name: "La Chouffe",
+    price: "3.60",
+  },
+  {
+    name: "Chimay",
+    price: "15.90",
+  },
+  {
+    name: "Riegele",
+    price: "6.70",
+  },
+  {
+    name: "Brewdog",
+    price: "3.50",
+  },
+  {
+    name: "La Trappe",
+    price: "3.80",
+  },
+  {
+    name: "Trappiste",
+    price: "35.90",
+  },
+  {
+    name: "Shepherd Neame",
+    price: "4.95",
+  },
+  {
+    name: "White Frontier",
+    price: "3.30",
+  },
+  {
+    name: "White Frontier",
+    price: "3.90",
+  },
+  {
+    name: "baladin",
+    price: "4.50",
+  },
+  {
+    name: "Brasserie Dupont",
+    price: "4.20",
+  },
+  {
+    name: "Stone",
+    price: "3.40",
+  },
+  {
+    name: "Chouffe",
+    price: "3.90",
+  },
+  {
+    name: "Corona",
+    price: "10.20",
+  },
+  {
+    name: "Bombardier",
+    price: "2.80",
+  },
+  {
+    name: "Wychwood",
+    price: "5.30",
+  },
+];
+var coop = [
+  {
+    name: "Augustiner Light Lager Beer 3x 50cl",
+    price: "7.80",
+    volume: "3x 50cl",
+  },
+  {
+    name: "BFM La Meule Bière à la sauge 4x33cl",
+    price: "19.20",
+    volume: "132cl",
+  },
+  {
+    name: "BFM La Salamandre Bier blanche 4x33cl",
+    price: "19.20",
+    volume: "132cl",
+  },
+  {
+    name: "Egger Spez Maximus Beer 6x33cl",
+    price: "8.50",
+    volume: "198cl",
+  },
+  {
+    name: "Guinness Hop House 13 Lager Beer 2x 33cl",
+    price: "5.90",
+    volume: "2x 33cl",
+  },
+  {
+    name: "Kirin Ichiban Japan Beer 3x 33cl",
+    price: "7.50",
+    volume: "3x 33cl",
+  },
+  {
+    name: "Peroni Bier Nastro Azzurro 4x33cl",
+    price: "7.60",
+    volume: "99cl",
+  },
+  {
+    name: "Pilgrim Waldbier Beer 3x 33cl",
+    price: "8.25",
+    volume: "3x 33cl",
+  },
+  {
+    name: "Rugenbräu Alpenperle Beer 10x33cl",
+    price: "13.95",
+    volume: "330cl",
+  },
+  {
+    name: "San Martino La Helles Beer 6x33cl",
+    price: "13.40",
+    volume: "198cl",
+  },
+  {
+    name: "San Martino La Nostra Beer 6x33cl",
+    price: "13.40",
+    volume: "198cl",
+  },
+  {
+    name: "San Martino Ticino Beer Box 6x33cl",
+    price: "13.95",
+    volume: "198cl",
+  },
+  {
+    name: "Ueli Light Lager Beer 10x33cl",
+    price: "15.95",
+    volume: "330cl",
+  },
+  {
+    name: "Aare Amber Beer 6x33cl",
+    price: "11.10",
+    volume: "198cl",
+  },
+  {
+    name: "Aare Kellerfrisch Lager Beer 6x33cl",
+    price: "11.10",
+    volume: "198cl",
+  },
+  {
+    name: "Appenzeller bière de tonneau en bois 3x 33cl",
+    price: "8.85",
+    volume: "3x 33cl",
+  },
+  {
+    name: "Appenzeller Quöllfrisch Beer 10x33cl",
+    price: "11.25",
+    volume: "330cl",
+  },
+  {
+    name: "Appenzeller Quöllfrisch Beer 12x25cl",
+    price: "12.50",
+    volume: "300cl",
+  },
+  {
+    name: "BFM Saison Surette Sour Beer",
+    price: "5.90",
+    volume: "37.5cl",
+  },
+  {
+    name: "Bier Factory Corp Monkey Lager 2x 33cl",
+    price: "7.90",
+    volume: "2x 33cl",
+  },
+  {
+    name: "Blanche de Namur Rosée Beer",
+    price: "5.95",
+    volume: "75cl",
+  },
+  {
+    name: "Bourgogne des Flandres Beer 2x 33cl",
+    price: "7.00",
+    volume: "2x 33cl",
+  },
+  {
+    name: "Boxer Brunette Beer 4x33cl",
+    price: "7.80",
+    volume: "132cl",
+  },
+  {
+    name: "Boxer Gourmande Beer 4x33cl",
+    price: "7.80",
+    volume: "132cl",
+  },
+  {
+    name: "Boxer Old Beer 10x33cl",
+    price: "10.95",
+    volume: "330cl",
+  },
+  {
+    name: "Boxer Old Beer 12x25cl",
+    price: "10.95",
+    volume: "300cl",
+  },
+  {
+    name: "Boxer Vedette Beer 4x33cl",
+    price: "7.80",
+    volume: "132cl",
+  },
+  {
+    name: "Brooklyn Lager Beer 4x35.5cl",
+    price: "11.30",
+    volume: "142cl",
+  },
+  {
+    name: "Calanda Glatsch Beer 10x33cl",
+    price: "11.30",
+    volume: "330cl",
+  },
+  {
+    name: "Cardinal non filtered 10x33CL",
+    price: "12.50",
+    volume: "330cl",
+  },
+  {
+    name: "Chopfab White Beer 10x33cl",
+    price: "12.95",
+    volume: "330cl",
+  },
+  {
+    name: "Coors Light Beer 3x 33cl",
+    price: "7.20",
+    volume: "3x 33cl",
+  },
+  {
+    name: "De Molen Hugs & Kisses Beer 2x 33cl",
+    price: "7.00",
+    volume: "2x 33cl",
+  },
+  {
+    name: "De Molen Rasputin Beer 2x 33cl",
+    price: "9.00",
+    volume: "2x 33cl",
+  },
+  {
+    name: "De Molen Vuur & Vlam Beer 2x 33cl",
+    price: "7.90",
+    volume: "2x 33cl",
+  },
+  {
+    name: "Docteur Gab's Pépite PA Beer 4x33cl",
+    price: "13.75",
+    volume: "132cl",
+  },
+  {
+    name: "Doppelleu Double IPA Beer 4x33cl",
+    price: "11.80",
+    volume: "132cl",
+  },
+  {
+    name: "Duvel Belgian Beer 2x 33cl",
+    price: "7.00",
+    volume: "2x 33cl",
+  },
+  {
+    name: "Eichhof Lager Beer 10x33cl",
+    price: "10.90",
+    volume: "330cl",
+  },
+  {
+    name: "Eichhof Retro Beer 9x33cl",
+    price: "10.20",
+    volume: "297cl",
+  },
+  {
+    name: "Eichhof Urfrisch Beer 9x33cl",
+    price: "10.95",
+    volume: "297cl",
+  },
+  {
+    name: "Einsiedler Organic Alpen Beer 6x33cl",
+    price: "11.95",
+    volume: "198cl",
+  },
+  {
+    name: "Feldschlösschen Win a Car Original Beer 18x33cl",
+    price: "14.95",
+    volume: "594cl",
+  },
+  {
+    name: "Felsenau Bärner Müntschi Beer 4x33cl",
+    price: "7.60",
+    volume: "132cl",
+  },
+  {
+    name: "Gluten Free Birra da Ris Beer 6x33cl",
+    price: "9.75",
+    volume: "198cl",
+  },
+  {
+    name: "Grimbergen Blanche Beer 6x25cl",
+    price: "11.50",
+    volume: "150cl",
+  },
+  {
+    name: "Heineken Beer 24x25cl",
+    price: "15.90",
+    volume: "600cl",
+  },
+  {
+    name: "Insel German Coast IPA Beer 2x 33cl",
+    price: "7.90",
+    volume: "2x 33cl",
+  },
+  {
+    name: "Insel Kreide Beer 2x 33cl",
+    price: "7.90",
+    volume: "2x 33cl",
+  },
+  {
+    name: "Kronenbourg 1664 Beer 12x25cl",
+    price: "11.95",
+    volume: "300cl",
+  },
+  {
+    name: "L'Echappée Originale Beer 2x37.5cl",
+    price: "7.80",
+    volume: "75cl",
+  },
+  {
+    name: "Lägere Bräu Beer 8x33cl",
+    price: "15.95",
+    volume: "264cl",
+  },
+  {
+    name: "La Nébuleuse Double Oat Beer 4x33cl",
+    price: "13.60",
+    volume: "132cl",
+  },
+  {
+    name: "La Nébuleuse Stirling Beer 4x33cl",
+    price: "13.20",
+    volume: "132cl",
+  },
+  {
+    name: "Leffe Lager Beer 3x 33cl",
+    price: "7.35",
+    volume: "3x 33cl",
+  },
+  {
+    name: "Liefmans Fruitesse Beer 3x 25cl",
+    price: "7.80",
+    volume: "3x 25cl",
+  },
+  {
+    name: "Liefmans Yell'Oh Beer 3x 25cl",
+    price: "8.40",
+    volume: "3x 25cl",
+  },
+  {
+    name: "Luzerner Original Beer 6x33cl",
+    price: "12.50",
+    volume: "198cl",
+  },
+  {
+    name: "Luzerner Red Ale 6x33cl",
+    price: "18.00",
+    volume: "198cl",
+  },
+  {
+    name: "Molson Canadian Beer 6x35.5cl",
+    price: "16.95",
+    volume: "213cl",
+  },
+];
+//define margins, height and width
+margin = { top: 20, right: 0, bottom: 30, left: 40 };
+height = 500;
+width = 954;
+var empty = 0;
+
+//create svg container
+const svg = d3
+  .select(".canva")
+  .append("svg")
+  .attr("viewBox", [0, 0, width, height]);
+//tooltip
+var tooltip = d3
+  .select(".canva")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+//data input and selection
+d3.select("#shop_option").on("change", change);
+
+function change() {
+  var data = coop;
+  if (this.selectedIndex == 1) {
+    data = manor;
+  }
+  //removing svg and tooltip
+  d3.select(".canva").select("svg").remove();
+  d3.select(".canva").select("div").remove();
+
+  //updating all elements
+  const svg = d3
+    .select(".canva")
+    .append("svg")
+    .attr("viewBox", [0, 0, width, height]);
+  var tooltip = d3
+    .select(".canva")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+  x = d3
+    .scaleBand()
+    .domain(data.map((d, i) => i))
+    .rangeRound([margin.left, width - margin.right])
+    .padding(0.1);
+  y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.price * 1.3)])
+    .range([height - margin.bottom, margin.top]);
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x));
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y));
+  //create x and y axis and define y axis title
+  yTitle = (g) =>
+    g
+      .append("text")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 11)
+      .attr("y", 10)
+      .text("Price in CHF");
+  yAxis = (g) =>
+    g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .call((g) => g.select(".domain").remove());
+  xAxis = (g) =>
+    g
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+  // create bars for each beer
+  var bars = svg
+    .append("g")
+    .attr("fill", "lightyellow")
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("x", (d, i) => x(i))
+    .attr("y", (d) => y(d.price))
+    .attr("height", empty)
+    .attr("width", x.bandwidth())
+    .on("mouseover", function (d) {
+      d3.select(this).style("fill", "yellow");
+      tooltip.transition().duration(200).style("opacity", 0.9);
+      tooltip
+        .html(d.name + "<br/> (" + d.price + "CHF)" + "<br />" + d.volume)
+        .style("left", d3.select(this) + "px")
+        .style("top", d3.select(this) - 50 + "px");
+    })
+    .on("mouseout", function (d) {
+      d3.select(this).style("fill", "lightyellow");
+      tooltip.transition().duration(500).style("opacity", 0);
+    });
+  bars
+    .transition()
+    .duration(1500)
+    .delay(200)
+    .attr("height", (d) => y(0) - y(d.price));
+  svg
+    .append("g")
+    .selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text("🍺")
+    .attr("x", (d, i) => x(i))
+    .attr("y", empty)
+    .on("mouseover", function (d) {
+      d3.select(this).style("fill", "red");
+      tooltip.transition().duration(200).style("opacity", 0.9);
+      tooltip
+        .html(d.name + "<br/> (" + d.price + "CHF)" + "<br />" + d.volume)
+        .style("left", d3.select(this) + "px")
+        .style("top", d3.select(this) - 50 + "px");
+    })
+    .on("mouseout", function (d) {
+      d3.select(this).style("fill", "steelblue");
+      tooltip.transition().duration(500).style("opacity", 0);
+    })
+    .transition()
+    .duration(1000)
+    .attr("y", (d) => y(d.price) + 14);
+
+  // call x, y axis and y title
+
+  svg.append("g").call(xAxis);
+
+  svg.append("g").call(yAxis);
+
+  svg.call(yTitle);
+}
+//define x and y scales and position them
+x = d3
+  .scaleBand()
+  .domain(coop.map((d, i) => i))
+  .rangeRound([margin.left, width - margin.right])
+  .padding(0.1);
+y = d3
+  .scaleLinear()
+  .domain([0, d3.max(coop, (d) => d.price * 1.3)])
+  .range([height - margin.bottom, margin.top]);
+
+svg
+  .append("g")
+  .attr("transform", `translate(0,${height - margin.bottom})`)
+  .call(d3.axisBottom(x));
+
+svg
+  .append("g")
+  .attr("transform", `translate(${margin.left},0)`)
+  .call(d3.axisLeft(y));
+//create x and y axis and define y axis title
+yTitle = (g) =>
+  g
+    .append("text")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 11)
+    .attr("y", 10)
+    .text("Price in CHF");
+yAxis = (g) =>
+  g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call((g) => g.select(".domain").remove());
+xAxis = (g) =>
+  g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+// create bars for each beer
+var bars = svg
+  .append("g")
+  .attr("fill", "lightyellow")
+  .selectAll("rect")
+  .data(coop)
+  .join("rect")
+  .attr("x", (d, i) => x(i))
+  .attr("y", (d) => y(d.price))
+  .attr("height", empty)
+  .attr("width", x.bandwidth())
+  .on("mouseover", function (d) {
+    d3.select(this).style("fill", "yellow");
+    tooltip.transition().duration(200).style("opacity", 0.9);
+    tooltip
+      .html(d.name + "<br/> (" + d.price + "CHF)" + "<br />" + d.volume)
+      .style("left", d3.select(this) + "px")
+      .style("top", d3.select(this) - 50 + "px");
+  })
+  .on("mouseout", function (d) {
+    d3.select(this).style("fill", "lightyellow");
+    tooltip.transition().duration(500).style("opacity", 0);
+  });
+bars
+  .transition()
+  .duration(1500)
+  .delay(200)
+  .attr("height", (d) => y(0) - y(d.price));
+
+//add beer emoji and on click function to display the data
+svg
+  .append("g")
+  .selectAll("text")
+  .data(coop)
+  .enter()
+  .append("text")
+  .text("🍺")
+  .attr("x", (d, i) => x(i))
+  .attr("y", empty)
+  .transition()
+  .duration(1000)
+  .attr("y", (d) => y(d.price) + 14);
+
+// call x, y axis and y title
+
+svg.append("g").call(xAxis);
+
+svg.append("g").call(yAxis);
+
+svg.call(yTitle);
